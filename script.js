2c9c01f2ae46bd38b3d2c1ed70c871d7634661d9
nxnnuh <angelina6521@outlook.com>
2025-10-18 18:58:19 -0400

    git commit -m "completed problem 1 created array then completed problem 2 printed array using for loop"

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
+let favoriteFoods = ["Spaghetti","Rice","Egg","Bacon","Ham","Potato"];
 
 
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
+for(let i = 0; i < favoriteFoods.length; i++) {
+  console.log(favoriteFoods[i]);
+}
 
 
 
